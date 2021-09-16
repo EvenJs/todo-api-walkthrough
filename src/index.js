5e9377d03c2f24305e04cc52da313a38469e3025
EvenW <yuhang0229@gmail.com>
2021-09-16 14:03:38 +1000

    add some packages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
+require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
+const morgan = require('morgan');
 const routes = require('./routes');
+const logger = require('./utils/logger');
 
 const app = express();
+
+const PORT = process.env.PORT || 9000;
+
+app.use(helmet());
+app.use(
+  process.env.NODE_ENV === 'production' ? morgan('common') : morgan('dev')
+);
 
 app.use(express.json());
 app.use(cors);
 
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log(`server listening on port 3000`);
+app.listen(PORT, () => {
+  logger.info(`server listening on port ${PORT}`);
 })
